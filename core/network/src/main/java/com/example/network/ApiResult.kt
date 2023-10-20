3475334fd7f8da8c971fa755fa9d50c300c2ae77
nevermind322 <alexfilonov2015@yandex.ru>
2023-10-20 22:26:37 +0300

    fix ApiResult

--- core/network/src/main/java/com/example/network/ApiResult.kt
+++ core/network/src/main/java/com/example/network/ApiResult.kt
@@ -1,6 +1,6 @@
 package com.example.network
 
-sealed class ApiResult<T : Any> {
+sealed class ApiResult< out T : Any> {
     data class Success<T : Any>(val data: T) : ApiResult<T>()
     data class Error(val code : Int,  val msg : String) : ApiResult<Nothing>()
     data class Exception(val e : Throwable) : ApiResult<Nothing>()
