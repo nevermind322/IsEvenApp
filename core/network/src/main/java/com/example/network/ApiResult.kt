822b2ef241b7203660cb6d8a00ae0759c4cbf900
nevermind322 <alexfilonov2015@yandex.ru>
2023-10-20 21:31:03 +0300

    add NumbersApiService

--- core/network/src/main/java/com/example/network/ApiResult.kt
+++ core/network/src/main/java/com/example/network/ApiResult.kt
@@ -1,8 +1,5 @@
 package com.example.network
 
-import com.example.model.IsEven
-
-
 sealed class ApiResult<T : Any> {
     data class Success<T : Any>(val data: T) : ApiResult<T>()
     data class Error<T : Any>(val code : Int,  val msg : String) : ApiResult<T>()
--- core/network/src/main/java/com/example/network/NetworkModule.kt
+++ core/network/src/main/java/com/example/network/NetworkModule.kt
@@ -2,16 +2,19 @@ package com.example.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
-const val BASE_URL = "https://api.isevenapi.xyz/api/"
+const val IS_EVEN_BASE_URL = "https://api.isevenapi.xyz/api/"
+const val NUMBERS_BASE_URL = ""
+const val IS_EVEN_API_QUALIFIER = "IsEven"
+const val NUMBERS_API_QUALIFIER = "Numbers"
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -24,16 +27,32 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun getRetrofit(moshi: Moshi): Retrofit =
+    @Named(IS_EVEN_API_QUALIFIER)
+    fun getIsEvenRetrofit(moshi: Moshi): Retrofit =
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(IS_EVEN_BASE_URL)
             .addCallAdapterFactory(ApiClassAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
 
     @Provides
     @Singleton
-    fun getApiService(retrofit: Retrofit): IsEvenApiService =
+    @Named(NUMBERS_API_QUALIFIER)
+    fun getNumbersApiRetrofit(moshi: Moshi): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(NUMBERS_BASE_URL)
+            .addCallAdapterFactory(ApiClassAdapterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    @Provides
+    @Singleton
+    fun getNumbersService(@Named(NUMBERS_API_QUALIFIER) retrofit: Retrofit): NumbersApiService =
+        retrofit.create(NumbersApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun getApiService(@Named(IS_EVEN_API_QUALIFIER) retrofit: Retrofit): IsEvenApiService =
         retrofit.create(IsEvenApiService::class.java)
 
 }
