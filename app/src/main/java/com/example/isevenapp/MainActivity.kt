f90251e9166aba82621e47ab2f47441bfee75049
nevermind322 <alexfilonov2015@yandex.ru>
2023-10-20 16:52:34 +0300

    added Navigation Drawer

--- app/src/main/java/com/example/isevenapp/MainActivity.kt
+++ app/src/main/java/com/example/isevenapp/MainActivity.kt
@@ -3,42 +3,41 @@ package com.example.isevenapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.text.isDigitsOnly
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.isevenapp.ui.theme.IsEvenAppTheme
 import com.example.keyboardInput.KEYBOARD_INPUT_ROUTE
-import com.example.keyboardInput.KeyboardInputScreen
 import com.example.keyboardInput.keyboardInputScreen
 import com.example.keyboardInput.navigateToKeyboardInput
-import com.example.sliderInput.SliderInputScreen
 import com.example.sliderInput.navigateToSliderInput
 import com.example.sliderInput.sliderInputScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -53,23 +52,69 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun IsEvenApp(navController: NavHostController = rememberNavController()) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val map = mapOf<String, () -> Unit>(
+        "Keyboard" to {
+            navController.navigateToKeyboardInput()
+            scope.launch { drawerState.close() }
+        },
+        "Slider " to {
+            navController.navigateToSliderInput()
+            scope.launch { drawerState.close() }
+        }
+    )
+
+    ModalNavigationDrawer(
+        drawerContent = { IsEvenDrawerSheet(clickActions = map) },
+        drawerState = drawerState
     ) {
-        Column {
-            IsEvenNavHost(navController = navController)
-            Row {
-                Button(onClick = { navController.navigateToKeyboardInput() }) { Text(text = "Keyboard") }
-                Spacer(modifier = Modifier.width(16.dp))
-                Button(onClick = { navController.navigateToSliderInput() }) { Text(text = "Slider") }
+        Scaffold(topBar = {
+            IsEvenTopAppBar {
+                scope.launch {
+                    if (drawerState.isOpen) drawerState.close() else drawerState.open()
+                }
             }
+        }) {
+            Column(modifier = Modifier.padding(it)) {
+                IsEvenNavHost(navController = navController)
+            }
+
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun IsEvenTopAppBar(onNavClick: () -> Unit) {
+
+    TopAppBar(title = { Text(text = "IsEven?") }, navigationIcon = {
+        Icon(
+            modifier = Modifier.clickable { onNavClick() },
+            imageVector = Icons.Default.Menu,
+            contentDescription = ""
+        )
+    })
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun IsEvenDrawerSheet(clickActions: Map<String, () -> Unit>) {
+
+    ModalDrawerSheet {
+        Column {
+            for ((screen, action) in clickActions) {
+                Button(onClick = action) { Text(text = screen) }
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
+
+}
 
 @Composable
 fun IsEvenNavHost(navController: NavHostController) {
