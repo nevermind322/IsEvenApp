c3062caceb81229a529a17546971d7d8a59f980c
nevermind322 <alexfilonov2015@yandex.ru>
2023-10-21 17:34:25 +0300

    detail screen changes

--- feature/numberList/src/main/java/com/example/listInput/DetailScreenUiState.kt
+++ feature/numberList/src/main/java/com/example/listInput/DetailScreenUiState.kt
@@ -1,12 +1,13 @@
 package com.example.listInput
 
 import com.example.model.IsEven
+import com.example.model.NumberFact
 
 sealed class DetailScreenUiState {
 
     data object Loading : DetailScreenUiState()
 
-    class Success(val data : IsEven) : DetailScreenUiState()
+    class Success(val data : NumberFact) : DetailScreenUiState()
 
     class Error(val msg : String) : DetailScreenUiState()
 }
--- feature/numberList/src/main/java/com/example/listInput/NumberDetailScreen.kt
+++ feature/numberList/src/main/java/com/example/listInput/NumberDetailScreen.kt
@@ -1,10 +1,39 @@
 package com.example.listInput
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.model.NumberFact
 
 @Composable
-fun NumberDetailScreen(number : Int ,vm   : NumberDetailViewModel = viewModel() ) {
-    Text(text = number.toString())
+fun NumberDetailScreen(number: Int, vm: NumberDetailViewModel = viewModel()) {
+
+    val state by vm.state.collectAsState()
+
+    LaunchedEffect(Unit) {
+        vm.getFact(number)
+    }
+
+    when (state) {
+        is DetailScreenUiState.Loading -> CircularProgressIndicator()
+        is DetailScreenUiState.Error -> Text(text = (state as DetailScreenUiState.Error).msg)
+        is DetailScreenUiState.Success -> NumberFact(fact = (state as DetailScreenUiState.Success).data)
+    }
+}
+
+
+@Composable
+fun NumberFact(fact: NumberFact) {
+
+    Column {
+        Text(text = fact.number.toString())
+        Text(text = fact.fact)
+    }
+
 }
