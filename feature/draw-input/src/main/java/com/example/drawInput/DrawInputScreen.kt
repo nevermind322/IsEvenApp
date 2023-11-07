30562fbaac4600ced37ab8272ebd82f3f2800f91
nevermind322 <alexfilonov2015@yandex.ru>
2023-11-07 17:44:47 +0300

    begin implement image saving

--- feature/draw-input/src/main/java/com/example/drawInput/DrawInputScreen.kt
+++ feature/draw-input/src/main/java/com/example/drawInput/DrawInputScreen.kt
@@ -1,8 +1,14 @@
 package com.example.drawInput
 
+import android.graphics.Bitmap
+import android.graphics.Paint
+import android.graphics.Picture
 import android.graphics.drawable.shapes.OvalShape
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
@@ -20,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.isSpecified
@@ -29,12 +36,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.createBitmap
+import androidx.core.graphics.record
 import kotlin.math.roundToInt
 
 @Composable
@@ -75,66 +89,96 @@ private enum class MotionEvent() {
 
 @Composable
 fun DrawCanvas() {
-
-
     val filter = PointerEventType.Move
     var offset by remember { mutableStateOf(Offset.Unspecified) }
-    var path by remember { mutableStateOf( Path()) }
+    var path by remember { mutableStateOf(Path()) }
     var motionEvent by remember { mutableStateOf(MotionEvent.Idle) }
+    var picture = remember { Picture() }
+
+    var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+
+    var width = remember { 0 }
+    var height = remember { 0 }
 
     Column {
 
         Button(onClick = {
             path = Path()
             offset = Offset.Unspecified
-        }) {}
+        }) { Text("clear") }
 
-        Canvas(modifier = Modifier
-            .fillMaxSize()
-            .pointerInput(filter) {
-                awaitEachGesture {
-                    awaitFirstDown().also {
-                        motionEvent = MotionEvent.Down
-                        offset = it.position
-                    }
-                    do {
-                        val event = awaitPointerEvent()
-                        event.changes.forEach { it.consume() }
-                        val change = event.changes.first()
-                        if (offset.isUnspecified) path.moveTo(
-                            change.position.x,
-                            change.position.y
-                        )
-                        offset = change.position
-                        motionEvent = MotionEvent.Move
-                    } while (event.changes.any { it.pressed })
-                    motionEvent = MotionEvent.Up
-                }
-            }, onDraw = {
-
-            when (motionEvent) {
-                MotionEvent.Idle -> Unit
-                MotionEvent.Move -> {
-                    if (offset.isSpecified)
-                        path.lineTo(offset.x, offset.y)
-                }
-
-                MotionEvent.Down -> path.moveTo(offset.x, offset.y)
-                MotionEvent.Up -> {
-                    path.lineTo(offset.x, offset.y)
-                    motionEvent = MotionEvent.Idle
-                }
+        Button(onClick = {
+            val picture = Picture()
+            picture.record(width, height) { drawPath(path.asAndroidPath(), Paint()) }
+            bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                Bitmap.createBitmap(picture)
+            } else {
+                val bitmap = Bitmap.createBitmap(
+                    picture.width,
+                    picture.height,
+                    Bitmap.Config.ARGB_8888
+                )
+                val canvas = android.graphics.Canvas(bitmap)
+                canvas.drawColor(android.graphics.Color.WHITE)
+                canvas.drawPicture(picture)
+                bitmap
             }
 
-            drawPath(
-                path,
-                Color.Red,
-                style = Stroke(
-                    width = 8.dp.toPx(),
-                    join = StrokeJoin.Round,
-                    cap = StrokeCap.Round
+        }) {
+            Text(text = "save")
+        }
+        if (bitmap != null)
+            Image(bitmap = bitmap!!.asImageBitmap(), contentDescription = "hui")
+        else
+            Canvas(modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(filter) {
+                    awaitEachGesture {
+                        awaitFirstDown().also {
+                            motionEvent = MotionEvent.Down
+                            offset = it.position
+                        }
+                        do {
+                            val event = awaitPointerEvent()
+                            event.changes.forEach { it.consume() }
+                            val change = event.changes.first()
+                            if (offset.isUnspecified) path.moveTo(
+                                change.position.x,
+                                change.position.y
+                            )
+                            offset = change.position
+                            motionEvent = MotionEvent.Move
+                        } while (event.changes.any { it.pressed })
+                        motionEvent = MotionEvent.Up
+                    }
+                }, onDraw = {
+
+                width = this.size.width.roundToInt()
+                height = this.size.height.roundToInt()
+
+                when (motionEvent) {
+                    MotionEvent.Idle -> Unit
+                    MotionEvent.Move -> {
+                        if (offset.isSpecified)
+                            path.lineTo(offset.x, offset.y)
+                    }
+
+                    MotionEvent.Down -> path.moveTo(offset.x, offset.y)
+                    MotionEvent.Up -> {
+                        path.lineTo(offset.x, offset.y)
+                        motionEvent = MotionEvent.Idle
+                    }
+                }
+
+                drawPath(
+                    path,
+                    Color.Red,
+                    style = Stroke(
+                        width = 8.dp.toPx(),
+                        join = StrokeJoin.Round,
+                        cap = StrokeCap.Round
+                    )
                 )
-            )
-        })
+            })
     }
 }
